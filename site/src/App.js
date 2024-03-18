1f24edaa32486ab2e2541fb09222da1e29f0b8f0
AdamSW1 <adamseanwinter@outlook.com>
2024-03-18 16:47:47 -0400

    added mission page

--- site/src/App.js
+++ site/src/App.js
@@ -16,12 +16,17 @@ import { Team } from './pages/team-page/Team.jsx'
 import { Home } from './pages/Home.jsx';
 import { Index } from './pages/Index.jsx';
 import { Media } from './pages/Media.jsx'
+import { Mission } from './pages/Mission.jsx'
 
 function App() {
 
   const [lang, setLang] = useState(localStorage.getItem('lang')?? 'EN');
 
   const router  = createBrowserRouter([
+    {
+      path: "/mission",
+      element: <Mission lang={lang} setLang={(lang) => {localStorage.setItem("lang", lang); setLang(lang); }}/>
+    },
     {
       path:"/media",
       element: <Media lang={lang} setLang={(lang) => {localStorage.setItem("lang", lang); setLang(lang); }}/>
