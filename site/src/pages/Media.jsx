7b44027269d5dca7ccbc1cf9aebe268dc1e0bec2
AdamSW1 <adamseanwinter@outlook.com>
2024-03-18 23:32:50 -0400

    new vid

--- site/src/pages/Media.jsx
+++ site/src/pages/Media.jsx
@@ -133,19 +133,14 @@ export function Media({lang, setLang}){
                         <a href="https://www.thelcars.com/license/">https://www.thelcars.com/license/</a>
                         <a href="https://lcars.computer/">https://lcars.computer/</a>
                         <h2>{vidAndTut}</h2>
-                        <iframe 
-                        width="560" height="315" 
-                        src="https://www.youtube.com/embed/D2QRyvPEanQ?si=gbMJ_CjvCXt6SkKM" 
+                        
+                        <iframe width="560" 
+                        height="315" 
+                        src="https://www.youtube.com/embed/ZN1RLZ01rqg?si=RZjNY9LCFh5ObpFY" 
                         title="YouTube video player" 
-                        frameborder="0"
-                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                         allowfullscreen></iframe>
-
-                            <iframe width="560" height="315"
-                                    src="https://www.youtube.com/embed/nelLwlKv3Gw?si=ZHBpO58Ioyhi1n3p"
-                                    title="YouTube video player" frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    allowFullScreen></iframe>
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
 
                         </section>
                         <Outlet/>
