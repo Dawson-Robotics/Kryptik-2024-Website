4c5ad5bdc1a4bbb0101c648179e072147655860e
Nathan Bokobza <nathan.bokobza@dawsoncollege.qc.ca>
2024-04-10 23:49:07 -0400

    Fix to F5 compilation

---
+++ site/finishbuild.bat
@@ -0,0 +1,17 @@
+cd build
+
+mkdir mission
+mkdir media
+mkdir articles
+mkdir crew
+mkdir index
+mkdir index/departments
+mkdir index/logs
+
+copy index.html mission
+copy index.html media
+copy index.html articles
+copy index.html crew
+copy index.html index
+copy index.html index/departments
+copy index.html index/logs
